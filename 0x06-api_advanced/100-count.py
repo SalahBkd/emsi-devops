7861b9817af0747e2b3a91319b40eacb18ae86a2
salahbkd <bkdsalah@gmail.com>
2021-07-04 19:12:55 +0100

    refactor

--- 0x06-api_advanced/100-count.py
+++ 0x06-api_advanced/100-count.py
@@ -1,41 +1,19 @@
 #!/usr/bin/python3
-
-"""[task 2, all the hot post]
-"""
+""" Recursive function that queries the Reddit API
+    parses the title of all hot articles, and prints a sorted count"""
 import requests
+import sys
+after = None
+count_dic = []
 
 
-def count_words(subreddit, word_list, after=None, count={}):
-    """[print 100 hot post]
-    Args:
-        subreddit ([string]): [type of subreddit to consult]
-        hot_list (list): [list of the titles]. Defaults to [].
-    """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    info = requests.get(url, headers={"User-Agent": "dairof7"},
-                        allow_redirects=False,
-                        params={'after': after})
-    if after is None:
-        for word in word_list:
-            count[word] = 0
-
-    if info.status_code == 200:
-        after = info.json()['data']['after']
-        if after is None:
-            for word, value in count.items():
-                if value != 0:
-                    print('{}: {}'.format(word, value))
-            return
-
-        for post in info.json()['data']['children']:
-            for word in word_list:
-                string = post['data']['title']
-                string_split = string.lower().split(' ')
-                count[word] += string_split.count(word.lower())
-        return count_words(subreddit, word_list, after, count)
-    else:
-        return
-
-
-if __name__ == "__main__":
-    pass
+def count_words(subreddit, word_list):
+    """parses the title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces) """
+    global after
+    global count_dic
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
